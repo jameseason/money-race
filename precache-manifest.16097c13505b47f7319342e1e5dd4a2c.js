f2b80e2b01f7ae4e73c57270e9dcc88b5c7b64e2
James Eason <JE053227@cerner.net>
2020-05-26 17:44:43 -0500

    Updates

---
+++ precache-manifest.16097c13505b47f7319342e1e5dd4a2c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3bf110cd2722ad7ac4437fbd1288b74",
+    "url": "/money-race/index.html"
+  },
+  {
+    "revision": "a830de60648241829028",
+    "url": "/money-race/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "095b059e79926086df29",
+    "url": "/money-race/static/js/2.15424ba6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/money-race/static/js/2.15424ba6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a830de60648241829028",
+    "url": "/money-race/static/js/main.e6e97ec4.chunk.js"
+  },
+  {
+    "revision": "150c06fe47cb26788288",
+    "url": "/money-race/static/js/runtime-main.c956a95a.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/money-race/static/media/logo.ee7cd8ed.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my-app/precache-manifest.fd474392c816b7ad9b735fcaf4c93936.js"
+  "/money-race/precache-manifest.16097c13505b47f7319342e1e5dd4a2c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/money-race/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });

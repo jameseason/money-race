afd2044e2c4803f3ae547543efca98aad8ad19d2
James Eason <JE053227@cerner.net>
2020-05-31 16:26:08 -0500

    Store api call

--- src/App.js
+++ src/App.js
@@ -10,21 +10,35 @@ class App extends Component {
         demId: 'S0ME00111',
         demName: 'Sara Gideon',
         repId: 'S6ME00159',
-        repName: 'Susan Collins',      
+        repName: 'Susan Collins',
+      },
+      {
+        state: 'North Carolina',
+        demId: 'S0NC00202',
+        demName: 'Cal Cunningham',
+        repId: 'S4NC00162',
+        repName: 'Thom Tillis',
+      },
+      {
+        state: 'Arizona',
+        demId: 'S0AZ00350',
+        demName: 'Mark Kelly',
+        repId: 'S8AZ00221',
+        repName: 'Martha McSally',
       },
       {
         state: 'Colorado',
         demId: 'S0CO00575',
         demName: 'John Hickenlooper',
         repId: 'S4CO00395',
-        repName: 'Cory Gardner',      
+        repName: 'Cory Gardner',
       }
     ]
   };
   
   render() {
     return (
-      <div>
+      <div className="raceList">
       {this.state.races.map(race => (
         <Race key={race.demId} race={race} />
       ))}
--- src/components/Race.js
+++ src/components/Race.js
@@ -2,7 +2,8 @@ import React from 'react';
 import NumberFormat from 'react-number-format';
 
 class Race extends React.Component {
-  
+// props: race: demId, repId, state
+
   state = {
     apiKey: '01S7pQx8JQPyWhG5lXIcG6QTgVh1ZQ87ITVH2gdH',
     demName: '',
@@ -12,8 +13,11 @@ class Race extends React.Component {
     repOnHand: 0,
     repTotal: 0
   };
-  
-  componentDidMount() {
+
+  /**
+  * Perform API calls to populate the state
+  */
+  callAPI() {
     // democrat
     fetch('https://api.open.fec.gov/v1/candidate/' + this.props.race.demId + '/totals/?cycle=2020&page=1&api_key=' + this.state.apiKey)
       .then(res => res.json())
@@ -35,11 +39,29 @@ class Race extends React.Component {
           repName: this.props.race.repName,
           repOnHand: data.results[0].last_cash_on_hand_end_period,
           repTotal: data.results[0].receipts
-        })
+        });
+        
+        // Store state after this call
+        const ls = require('localstorage-ttl');
+        ls.set(this.props.race.demId, JSON.stringify(this.state), 84000000);
       })
       .catch(console.log)  
   }
   
+  componentDidMount() {
+
+    const ls = require('localstorage-ttl');
+    const s = ls.get(this.props.race.demId);
+
+    if (s != null) {
+      this.setState(JSON.parse(s));
+      console.log('grabbed from storage '.concat(this.props.race.demId));
+    } else {
+      this.callAPI();
+      console.log('performed api call for '.concat(this.props.race.demId));
+    }
+  }
+
   render() {
     return (
       <div>

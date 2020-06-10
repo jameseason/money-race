4d2ffafab8d801613f9890863d3975cf7df1ca91
James Eason <JE053227@cerner.net>
2020-06-10 12:01:01 -0500

    Improve controller display

--- src/components/Controller.js
+++ src/components/Controller.js
@@ -8,42 +8,46 @@ class Controller extends React.Component {
     return(
       <div>
         {/* Cash on hand vs total raised toggle */}
-        <div className="btn-group btn-group-toggle" data-toggle="buttons" onChange={event => this.props.onHandHandler(event)}>
-          <label className={`btn btn-secondary ${this.props.onHand ? "active" : ""}`}>
-            <input type="radio" value="true" name="onHandButton" id="cashOnHand" autoComplete="off" defaultChecked />
-            Cash on Hand
-          </label>
-          <label className={`btn btn-secondary ${this.props.onHand ? "" : "active"}`}>
-            <input type="radio" value="false" name="onHandButton" id="cashTotal" autoComplete="off" />
-            Total Raised
-          </label>
+        <div className = "controller-element">
+        Value to display: <br />
+          <div className="btn-group btn-group-toggle" data-toggle="buttons" onChange={event => this.props.onHandHandler(event)}>
+            <label className={`btn btn-secondary ${this.props.onHand ? "active" : ""}`}>
+              <input type="radio" value="true" name="onHandButton" id="cashOnHand" autoComplete="off" defaultChecked />
+              Cash on Hand
+            </label>
+            <label className={`btn btn-secondary ${this.props.onHand ? "" : "active"}`}>
+              <input type="radio" value="false" name="onHandButton" id="cashTotal" autoComplete="off" />
+              Total Raised
+            </label>
+          </div>
         </div>
         
         {/* Filter by race rating */}
-        <div>
-          <p>Race ratings to include: <br />
+        <div className = "controller-element">
+          Race ratings to include: <br />
           <button type="button" className={`btn btn-${this.props.includeTossup ? "secondary active" : "outline-secondary"}`} 
-                  onClick={event => this.props.toggleTossup(event)}>Tossup</button>
+                  onClick={event => this.props.toggleTossup(event)}>Tossup</button>&nbsp;
           <button type="button" className={`btn btn-${this.props.includeLean ? "secondary active" : "outline-secondary"}`} 
-                  onClick={event => this.props.toggleLean(event)}>Lean</button>
+                  onClick={event => this.props.toggleLean(event)}>Lean</button>&nbsp;
           <button type="button" className={`btn btn-${this.props.includeLikely ? "secondary active" : "outline-secondary"}`} 
-                  onClick={event => this.props.toggleLikely(event)}>Likely</button>
+                  onClick={event => this.props.toggleLikely(event)}>Likely</button>&nbsp;
           <button type="button" className={`btn btn-${this.props.includeSolid ? "secondary active" : "outline-secondary"}`} 
                   onClick={event => this.props.toggleSolid(event)}>Solid</button>
-          </p>
         </div>
         
         {/* Sort by */}
-          Sort by: <br />
-        <div className="btn-group btn-group-toggle" data-toggle="buttons" onChange={event => this.props.sortByHandler(event)}>
-          <label className={`btn btn-secondary ${this.props.sortBy === "state" ? "active" : ""}`}>
-            <input type="radio" value="state" name="sortByButton" id="stateNameSort" autoComplete="off" defaultChecked />
-            State name
-          </label>
-          <label className={`btn btn-secondary ${this.props.sortBy === "rating" ? "active" : ""}`}>
-            <input type="radio" value="rating" name="sortByButton" id="raceRatingSort" autoComplete="off" />
-            Race rating
-          </label>
+        <div className = "controller-element">
+        Sort by: <br />
+          <div className="btn-group btn-group-toggle" data-toggle="buttons" onChange={event => this.props.sortByHandler(event)}>
+            <label className={`btn btn-secondary ${this.props.sortBy === "state" ? "active" : ""}`}>
+              <input type="radio" value="state" name="sortByButton" id="stateNameSort" autoComplete="off" defaultChecked />
+              State name
+            </label>
+            <label className={`btn btn-secondary ${this.props.sortBy === "rating" ? "active" : ""}`}>
+              <input type="radio" value="rating" name="sortByButton" id="raceRatingSort" autoComplete="off" />
+              Race rating
+            </label>
+          </div>
         </div>
       </div>
     )
